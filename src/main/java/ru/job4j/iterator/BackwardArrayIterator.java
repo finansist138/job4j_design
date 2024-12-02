23b09f6a0a9eff8c98f650bd68b004ed224b2805
finansist138 <103438132+finansist138@users.noreply.github.com>
2024-12-02 13:46:18 +0300

    1. Что такое итератор. [#4951 #523949]

--- src/main/java/ru/job4j/iterator/BackwardArrayIterator.java
+++ src/main/java/ru/job4j/iterator/BackwardArrayIterator.java
@@ -7,16 +7,13 @@ public class BackwardArrayIterator implements Iterator<Integer> {
     private int point;
 
     public BackwardArrayIterator(int[] data) {
-        for (int i = 0; i < data.length / 2; i++) {
-            int tmp = data[i];
-            data[i] = data[data.length - 1 - i];
-            data[data.length - 1 - i] = tmp;
-        }
+
         this.data = data;
     }
 
     @Override
     public boolean hasNext() {
+
         return point < data.length;
     }
 
@@ -24,6 +21,13 @@ public class BackwardArrayIterator implements Iterator<Integer> {
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException("point >= data.length");
+        }
+        if (point == 0) {
+            for (int i = 0; i < data.length / 2; i++) {
+                int tmp = data[i];
+                data[i] = data[data.length - 1 - i];
+                data[data.length - 1 - i] = tmp;
+            }
         }
         return data[point++];
     }
