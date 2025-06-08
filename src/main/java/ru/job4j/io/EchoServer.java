fe9f629999e95a29c38236904b64aed020b6a02c
finansist138 <103438132+finansist138@users.noreply.github.com>
2025-06-08 22:07:14 +0300

    3. Бот [#7921 #545397]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,8 +14,12 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String string = input.readLine();
-                        if (string != null && string.contains("msg=Bye")) {
+                        if (string != null && string.contains("msg=Exit")) {
                             server.close();
+                        } else if (string != null && string.contains("msg=Hello")) {
+                            output.write("Hello, dear friend.\r\n\r\n".getBytes());
+                        } else {
+                            output.write("What.\r\n\r\n".getBytes());
                         }
                         System.out.println(string);
 
